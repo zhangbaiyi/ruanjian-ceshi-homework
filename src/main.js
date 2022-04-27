a317023b4880936ce4f53ba0cdf1fd266005d6ed
zhangbaiyi <zbaiyi@outlook.com>
2022-04-27 21:05:53 +0800

    implemented file testing

--- src/main.js
+++ src/main.js
@@ -4,10 +4,30 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
 import http from 'axios';
+import * as echarts from 'echarts/lib/echarts';
 
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { PieChart } from "echarts/charts";
+import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent
+} from "echarts/components";
+
+use([
+  CanvasRenderer,
+  PieChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent
+]);
+
+Vue.use(echarts)
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 
+Vue.prototype.$echarts = echarts
 Vue.prototype.$http = http
 
 new Vue({
